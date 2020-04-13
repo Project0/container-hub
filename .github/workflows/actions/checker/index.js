73971ca335ad3ee076c17ab63ac713b415dea65c
Richard Hillmann <richie@project0.de>
2020-04-13 13:08:58 +0200

    Add checker action

---
+++ .github/workflows/actions/checker/index.js
@@ -0,0 +1,85 @@
+const core = require('@actions/core');
+
+const { graphql } = require("@octokit/graphql");
+
+const { createActionAuth } = require("@octokit/auth-action");
+const actionAuth = createActionAuth()
+const auth = await actionAuth()
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `${auth.type} ${auth.token}`
+  }
+});
+
+
+
+const queryTagsAndReleases = `{
+  repository(name: "exim", owner: "exim") {
+    name
+     tags: refs(refPrefix: "refs/tags/", last: 30) {
+      edges {
+        node {
+          target {
+            ... on Tag {
+              name
+              target {
+                ... on Commit {
+                  committedDate
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    releases(last: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+      nodes {
+        name
+        createdAt
+        url
+      }
+    }
+  }
+}`;
+
+async function run() {
+  try {
+    repoOwner = core.getInput('owner', {required: true});
+    repoName = core.getInput('name', {required: true});
+    originVersion = core.getInput('version', {required: true});
+    versionMatch = core.getInput('match', {required: false});
+    core.info(`Check for new releases in ${repoOwner}/${repoName} against ${originVersion}`)
+
+    const { lastVersions } = await graphqlWithAuth({
+      query: `query lastVersions($owner: String!, $repo: String!, $num: Int = 100) {
+        repository(owner:$owner, name:$repo) {
+          tags: refs(refPrefix: "refs/tags/", last: $num) {
+            edges {
+              node {
+                target {
+                  ... on Tag {
+                    name
+                    target {
+                      ... on Commit {
+                        committedDate
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`,
+      owner: repoOwner,
+      repo: repoName
+    })
+
+    core.debug(lastVersions)
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
